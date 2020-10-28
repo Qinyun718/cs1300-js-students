eb86c7da3433a388809b8880d9fd9f9e9d81fe58
qinyun718 <qinyun_wu@brown.edu>
2020-10-27 20:21:03 -0400

    submission

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=-oGIeFlTkCc7N3oYzSZfRjaNNGe85V8kL0TctH3UUxo";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -20,10 +20,23 @@ const corsPromise = () =>
     resolve(request);
   });
 
+
+
+
+
 // THIS IS SOME SAMPLE CODE FOR HOW TO USE PROMISES -- feel free to adapt this into a function!
 corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
+      plants = JSON.parse(request.response).data
+      console.log(plants)
+      newElement = ""
+      for (var i in plants){
+        newElement += '<h1>' + "Name: " + plants[i].common_name + '</h1>' + 
+        '<h2>' + "Family:" + plants[i].family + '</h2>'
+        
+      }
+      document.getElementById("plant").innerHTML = newElement;
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
     })
 );
